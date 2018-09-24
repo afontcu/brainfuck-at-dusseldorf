c4c22faa8e016d3680a6afd6a696906a2a10eef7
Adrià Fontcuberta <adria@calidae.com>
2018-09-24 20:07:42 +0200

    Moar stuff

--- theme.js
+++ theme.js
@@ -3,43 +3,7 @@ import style from 'react-syntax-highlighter/styles/prism/solarizedlight'
 
 export default {
   ...theme,
-  prism: {
-    style
-  },
-  // css: {
-  //   ...theme.css,
-  //   textAlign: 'left',
-  //   blockquote: {
-  //     paddingLeft: '1em',
-  //     paddingRight: '1em',
-  //   },
-  //   'blockquote > p': {
-  //     lineHeight: '1.2',
-  //     fontSize: '1.2em',
-  //     paddingLeft: '2em',
-  //     paddingRight: '2em',
-  //   },
-  //   'blockquote + p': {
-  //     paddingRight: '2em',
-  //     textAlign: 'right',
-  //   },
-  //   li: {
-  //     fontSize: '1.4em',
-  //     paddingBottom: '1.2em',
-  //   },
-  // },
-  // heading: {
-  //   textTransform: 'inherit',
-  // },
-  // colors: {
-  //   text: '#333',
-  //   background: '#fafafa',
-  //   link: '#1c7cc2',
-  // },
-  // p: {
-  //   fontSize: '1.5em',
-  //   lineHeight: '1.4',
-  // },
+  prism: { style },
   link: {
     color: '#1c7cc2'
   },
